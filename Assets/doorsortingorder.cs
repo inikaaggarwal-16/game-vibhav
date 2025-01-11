a9a9e9cee0198e599982882955a36bb740ea3ffe
inikaaggarwal-16 <inikaaggarwal10@gmail.com>
2025-01-11 14:14:30 +0530

    q
    a

---
+++ Assets/doorsortingorder.cs
@@ -0,0 +1,68 @@
+/*using UnityEngine;
+
+public class DoorSortingOrder : MonoBehaviour
+{
+    public GameObject player; 
+    public GameObject door;   
+
+    private SpriteRenderer doorSpriteRenderer; 
+
+    void Start()
+    {
+        
+        doorSpriteRenderer = door.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        
+        float playerY = player.transform.position.y;
+        float doorY = door.transform.position.y;
+
+         
+        if (playerY < doorY)
+        {
+            
+            doorSpriteRenderer.sortingOrder = 0;
+        }
+        
+        else if (doorY < playerY)
+        {
+            
+            doorSpriteRenderer.sortingOrder = 1;
+        }
+    }
+} */
+
+using UnityEngine;
+
+public class DoorSortingOrder : MonoBehaviour
+{
+    public GameObject player; 
+    public GameObject door;   
+
+    private SpriteRenderer doorSpriteRenderer; 
+
+    void Start()
+    {
+        
+        doorSpriteRenderer = door.GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        
+        float playerY = player.transform.position.y;
+        float doorY = door.transform.position.y;
+
+        if (playerY < doorY)
+        {
+            doorSpriteRenderer.sortingLayerName = "SpecialLight";
+        }
+        else
+        {
+            doorSpriteRenderer.sortingLayerName = "doors";
+        }
+    }
+}
+
